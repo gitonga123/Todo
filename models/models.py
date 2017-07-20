869b8567eb211ca1f600bf1a4aad84f67db605c9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2017-07-20 14:18:17 +0300

    The Business Logic Layer: Add Busines Logic - oe_highlight

--- models/models.py
+++ models/models.py
@@ -10,3 +10,25 @@ class TodoTask(models.Model):
 	is_done=fields.Boolean("Done?")
 	active=fields.Boolean('Active?',default=True)
 
+	@api.multi
+	def do_toggle_done(self):
+		for task in self:
+			task.is_done = not task.is_done
+		return True
+
+	@api.multi 
+	def do_clear_done(self):
+		dones = self.search([('is_done','=',True)])
+		dones.write({'active':False})
+
+		return True
+
+	@api.multi
+	def do_toggle_active(self):
+		for task in self:
+			task.active = not task.active
+		return True
+
+
+		
+
